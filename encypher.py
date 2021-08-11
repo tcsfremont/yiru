f68812fcbb64d1510299d771270c0cb686f1d397
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-11 09:15:33 -0700

    cleaning data

---
+++ encypher.py
@@ -0,0 +1,30 @@
+from sys import argv as arguements
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+if len(arguements) != 3:
+
+    print("2 arguents required!") 
+    exit()
+
+from_filename = arguements[1]
+to_filename = arguements [2]
+
+print("input: " + from_filename)
+print("output: " + to_filename)
+
+from_file = open(from_filename, 'r', encoding='utf8')
+to_file = open(to_filename, 'w', encoding='utf8')
+
+contents = from_file.read()
+
+contents = [letter if letter.up() in ALPHABET else '' for letter in contents]
+
+contents = ''.join(contents)
+
+contents = contents.upper()
+
+to_file.write(contents)
+
+from_file.close()
+to_file.close()
--- frequency.py
+++ frequency.py
@@ -1,9 +1,40 @@
 import sys
 
-text = ' '.join(sys.argv[1:])
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+args = sys.argv[1:]
+
+for arg in args:
+
+    print(arg)
+
+text = ''.join(sys.argv[1:])
+
+print(text)
+
+lowercase_letter = text[0].lower()
+
+print(lowercase_letter)
+
+lowercase_text = [letter.lower() for letter in text]
+
+print(lowercase_text)
+
+lowercase_text = [letter if letter in ALPHABET else '' for leter om lowercase_text] 
+
+print(lowercase_letter)
+
+lowercase_text = ''.join(lowercase_text)
+
+print(lowercase_text)
+
 frequencies = {}
 
-for letter in text:
+for letter in ALPHABET:
+
+    frequencies[letter] = 0
+
+for letter in lowercase_text:
 
     if letter not in frequencies: 
 
@@ -14,3 +45,15 @@ for letter in text:
         frequencies[letter] += 1
 
 print(frequencies)
+
+for letter in ALPHABET:
+
+    count = frequencies[letter]
+
+    print(letter, end= ' ')
+
+    for c in range(count):
+
+        print('|', end='')
+
+    print()
