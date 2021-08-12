978827e1c77de471c5449efb39d5e59c27c94475
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-12 13:04:49 -0700

    encypher

--- analyze.py
+++ analyze.py
@@ -1,11 +1,10 @@
 from sys import argv as arguements
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-KEY = 'EOVRTYUQHPASXFNWDKZLJCIBGM';
 
 if len(arguements) != 3:
 
-    print("3 arguents required!") 
+    print("2 arguents required!") 
     exit()
 
 from_filename = arguements[1]
@@ -17,21 +16,15 @@ print("output: " + to_filename)
 from_file = open(from_filename, 'r', encoding='utf8')
 to_file = open(to_filename, 'w', encoding='utf8')
 
-contents = list(from_file.read())
+contents = from_file.read()
 
-for index in range(len(contents)):
+contents = [letter if letter.upper() in ALPHABET else '' for letter in contents]
 
-    letter = contents[index]
-    order = ord(letter) - ord('A')
+contents = ''.join(contents)
 
-    if order < 0 or order >= len(KEY):
+contents = contents.upper()
 
-        continue
-
-    cipher_letter = KEY[order]
-    contents[index] = cipher_letter
-
-to_file.write(''.join(contents))
+to_file.write(contents)
 
 from_file.close()
 to_file.close()
