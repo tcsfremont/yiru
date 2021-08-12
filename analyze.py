0490406d88f4be8cba325c25b6cea0854ad832d3
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-12 14:40:13 -0700

    analyzing

--- analyze.py
+++ analyze.py
@@ -2,29 +2,52 @@ from sys import argv as arguements
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-if len(arguements) != 3:
+if len(arguements) != 2:
 
-    print("2 arguents required!") 
+    print("1 arguement required!") 
     exit()
 
 from_filename = arguements[1]
-to_filename = arguements [2]
+#to_filename = arguements [2]
 
 print("input: " + from_filename)
-print("output: " + to_filename)
+#print("output: " + to_filename)
 
 from_file = open(from_filename, 'r', encoding='utf8')
-to_file = open(to_filename, 'w', encoding='utf8')
+#to_file = open(to_filename, 'w', encoding='utf8')
 
 contents = from_file.read()
 
-contents = [letter if letter.upper() in ALPHABET else '' for letter in contents]
+def analyze(contents):
 
-contents = ''.join(contents)
+    length = float(len(contents))
 
-contents = contents.upper()
+    frequencies = {}
 
-to_file.write(contents)
+    for letter in ALPHABET:
+
+        frequencies[letter] = 0.0
+
+    for letter in contents:
+
+        frequencies[letter] += 1.0
+
+    ratios = {}
+
+    for letter in ALPHABET: 
+
+        ratios[letter] = frequencies[letter] / length
+
+    return ratios
+
+ratios = analyze(contents)
+
+for letter in ALPHABET:
+
+    print(letter, end=' ')
+    print(ratios[letter])
+
+#to_file.write(contents)
 
 from_file.close()
-to_file.close()
+#to_file.close()
--- crack.py
+++ crack.py
@@ -1,11 +1,11 @@
 from sys import argv as arguements
+from analyze import analyze
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-KEY = 'EOVRTYUQHPASXFNWDKZLJCIBGM'
 
 if len(arguements) != 3:
 
-    print("3 arguents required!") 
+    print("2 arguents required!") 
     exit()
 
 from_filename = arguements[1]
@@ -17,21 +17,9 @@ print("output: " + to_filename)
 from_file = open(from_filename, 'r', encoding='utf8')
 to_file = open(to_filename, 'w', encoding='utf8')
 
-contents = list(from_file.read())
+contents = from_file.read()
 
-for index in range(len(contents)):
-
-    letter = contents[index]
-    order = ord(letter) - ord('A')
-
-    if order < 0 or order >= len(KEY):
-
-        continue
-
-    cipher_letter = KEY[order]
-    contents[index] = cipher_letter
-
-to_file.write(''.join(contents))
+to_file.write(contents)
 
 from_file.close()
 to_file.close()
